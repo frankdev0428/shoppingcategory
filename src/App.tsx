229ed38d8919913eedf6efdccc3e9a0e6091b663
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-08-17 16:50:58 -0700

    dataModify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import SliderComponent from "./slider-cart/SliderComponent" ;
 import dataCard from "./slider-cart/data";
 import styles from '../src/App.module.css'
+import DataModify from "./modify/DataModify";
+
 function App() {
    
   return (
     <div className={styles.app}>
+      <div className="dataModify">
+        <DataModify />
+      </div>
       <h1 className= {styles.title} 
       >This is Slider Collection</h1>
       <SliderComponent cardData={dataCard} />
---
+++ src/modify/DataModify.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DataModify = () => {
+    const X = [
+        
+    ]
+  return (
+    <div>
+     <ul>
+      {X.map(x => <li>{x}</li>)}
+     </ul>
+    </div>
+  )
+}
+
+export default DataModify
