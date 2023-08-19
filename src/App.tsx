2cad7917727bb47f3d2eb1f273bb05f9a8f3b8b3
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-08-19 11:07:59 -0700

    data isLoading

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import styles from '../src/App.module.css'
 import DataModify from "./modify/DataModify";
 import Footer from "./footer/Footer";
 import Navbar from "./Navbar/Navbar";
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 import { useEffect , useState } from 'react';
 
 interface User {
@@ -15,18 +15,32 @@ interface User {
 function App() {
   const [users,setUsers] = useState<User[]>([]) ;
   const [error,setError] = useState('');
-  
-  useEffect(() => {
+  const [isLoading , setLoading] = useState(false);
+  useEffect( () => {
+    const controller = new AbortController();
+    setLoading(true) ;
+
     axios
-    .get<User[]>('https://jsonplaceholder.typicode.com/xusers')
-    .then(res => setUsers(res.data))
-    .catch(err => setError(err.message));
+     .get<User[]>('https://jsonplaceholder.typicode.com/users',{ signal: controller.signal })
+     .then(res => {
+      setUsers(res.data);
+      setLoading(false);
+    })
+     .catch((err) => {
+      if (err instanceof CanceledError) return;
+      setError(err.message);
+      setLoading(false);
+    })
+   
+    return () => controller.abort();
   }, [])
 
   return (
     <div className={styles.app}>
       <Navbar />
       <div className="User">
+       { isLoading && <div className="spinner-border"></div>}
+        
         <p className="text-danger">{error}</p>
         <ul>
         {users.map(user => <li key={user.id}>
