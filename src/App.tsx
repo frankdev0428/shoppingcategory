3fd1f40f61576514164735f692c1b67b97d04b15
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-08-27 16:35:01 -0700

    modify user API

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,19 @@ function App() {
     const newUser = {id: 0,name: 'Hoang'};
     setUsers([newUser,...users]);
     axios.post('https://jsonplaceholder.typicode.com/users',newUser)
-    .then(res => setUsers([res.data,...users]))
+    .then(({data: savedUser}) => setUsers([savedUser,...users]))
+  }
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = {...user, name: user.name  + '!'};
+    setUsers(users.map( u => u.id === user.id ? updatedUser : u))
+
+    axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser)
+    .catch(err => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
   }
   return (
     <div className={styles.app}>
@@ -59,7 +71,10 @@ function App() {
         <ul className="list-group">
         {users.map(user => <li className="list-group-item d-flex justify-content-between" key={user.id}>
          {user.name} 
+        <div>
+        <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
         <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+        </div>
         </li>)}
         </ul>
       </div>
